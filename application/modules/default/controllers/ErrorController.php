2947eff0cc9b9c5be1f25be312d19e81f58be371
Oleksandr Khutoretsky <olekhy@googlemail.com>
2011-05-16 10:01:27 +0200

    Lightweight refactoring and adding folder for models in the framework structure. Added login action helper and their components.

--- application/modules/default/controllers/ErrorController.php
+++ application/modules/default/controllers/ErrorController.php
@@ -1,6 +1,6 @@
 <?php
 
-class ErrorController extends Zend_Controller_Action
+class ErrorController extends App_Core_Controller_Action
 {
 
     public function errorAction()
--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,63 +1,67 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
+class IndexController extends App_Core_Controller_Action
 {
 
+    
     public function init()
     {
         /* Initialize action controller here */
-
-        $this->_helper->login();
+        //$this->setSecureActions(array('index'));
+        $this->view->BASETAG = $this->serverNameWithProtocol();
+        //$this->_helper->login();
     }
-
-    public function indexAction()
-    {
-        print "<pre/>";
+    /**
+     * @return void
+     */
+    //public function indexAction()
+    //{
+        //print "<pre/>";
         //var_dump($_SESSION);
         //var_dump($this->getRequest()->getParams());
         //var_dump($this->_getAllParams());  die;
         
-        $log = $this->_invokeArgs['bootstrap']->getResource('Log');
-        $log->debug('hallo');
+        //$log = $this->_invokeArgs['bootstrap']->getResource('Log');
+        //$log->debug('hallo');
 
-        $cacheManager = $this->_invokeArgs['bootstrap']->getResource('CacheManager');
+        //$cacheManager = $this->_invokeArgs['bootstrap']->getResource('CacheManager');
         /** @var $cacheManager Zend_Cache_Manager */
 
 
-        $cacheCore = $cacheManager->getCache('core');
+        //$cacheCore = $cacheManager->getCache('core');
 
-        if(!($cached = $cacheCore->load('test')))
-        {
-            $cached = time();
-            $cacheCore->save($cached, 'test');
-        }
+        //if(!($cached = $cacheCore->load('test')))
+        //{
+        //    $cached = time();
+        //    $cacheCore->save($cached, 'test');
+        //}
 
-        var_dump($cached);
+        //var_dump($cached);
 
-        $cacheFile = $cacheManager->getCache('onfile');
-        $file = APPLICATION_PATH.'/../tmp/test.php';
+        //$cacheFile = $cacheManager->getCache('onfile');
+        //$file = APPLICATION_PATH.'/../tmp/test.php';
 
-        if(method_exists($cacheFile, 'setMasterFiles'))
-        {
-            $cacheFile->setMasterFiles(array($file));
-        }
+        //if(method_exists($cacheFile, 'setMasterFiles'))
+        //{
+        //    $cacheFile->setMasterFiles(array($file));
+        //}
 
-        if(!($cached1 = $cacheFile->load('test1')))
-        {
-            $cached1 = include $file;
-            $cacheFile->save($cached1, 'test1');
-        }
+        //if(!($cached1 = $cacheFile->load('test1')))
+        //{
+        //    $cached1 = include $file;
+        //    $cacheFile->save($cached1, 'test1');
+        //}
 
-        var_dump($cached1);
+        //var_dump($cached1);
 
-        $cacheMemcached = $cacheManager->getCache('memcached');
+        //$cacheMemcached = $cacheManager->getCache('memcached');
         //var_export($cacheMemcached);
-        if(!($cached2 = $cacheMemcached->load('test2')))
-        {
-            $cached2 = time();
-            $cacheMemcached->save($cached2, 'test2');
-        }
-        var_dump($cached2);
+        //if(!($cached2 = $cacheMemcached->load('test2')))
+        //{
+        //    $cached2 = time();
+        //    $cacheMemcached->save($cached2, 'test2');
+        //}
+        //var_dump($cached2);
         //Zend_Session::start();
         //$sess = new Zend_Session_Namespace();
         //if(!isset($sess->hallo)){
@@ -70,13 +74,20 @@ class IndexController extends Zend_Controller_Action
         //var_dump($translate->getAdapter()->getLocale());
         //var_dump(get_class_methods($translate->getAdapter()));
         //$this->view->hallo = "HALLO";
-    }
+    //}
 
-    public function localeAction()
+    //public function localeAction()
+    //{
+    //    print "<pre/>";
+    //    var_dump($this->_getAllParams());
+    //}
+
+    /**
+     * @return void
+     */
+    public function indexAction()
     {
-        print "<pre/>";
-        var_dump($this->_getAllParams());
+        
     }
-
 }
 
